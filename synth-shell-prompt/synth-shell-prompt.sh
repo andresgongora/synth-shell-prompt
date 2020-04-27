f0997350f8e4a7febd4d4670480fae159b1770b2
Andres Gongora <mail@andresgongora.com>
2020-04-27 11:53:14 +0200

    Add optional segment padding
    Signed-off-by: Andres Gongora <mail@andresgongora.com>

--- synth-shell-prompt/synth-shell-prompt.sh
+++ synth-shell-prompt/synth-shell-prompt.sh
@@ -68,10 +68,10 @@ synth_shell_prompt()
 ##	By default, these symbols and their meaning are (set globally):
 ##
 ##		UPSTREAM	NO CHANGE		DIRTY
-##		up to date	FBP_GIT_SYNCED		FBP_GIT_DIRTY
-##		ahead		FBP_GIT_AHEAD		FBP_GIT_DIRTY_AHEAD
-##		behind		FBP_GIT_BEHIND		FBP_GIT_DIRTY_BEHIND
-##		diverged	FBP_GIT_DIVERGED	FBP_GIT_DIRTY_DIVERGED		
+##		up to date	SSP_GIT_SYNCED		SSP_GIT_DIRTY
+##		ahead		SSP_GIT_AHEAD		SSP_GIT_DIRTY_AHEAD
+##		behind		SSP_GIT_BEHIND		SSP_GIT_DIRTY_BEHIND
+##		diverged	SSP_GIT_DIVERGED	SSP_GIT_DIRTY_DIVERGED		
 ##
 ##	Returns an empty string otherwise.
 ##
@@ -112,21 +112,21 @@ getGitBranch()
 
 			## SELECT SYMBOL
 			if   $is_dirty && $is_ahead && $is_behind; then
-				local symbol=$FBP_GIT_DIRTY_DIVERGED
+				local symbol=$SSP_GIT_DIRTY_DIVERGED
 			elif $is_dirty && $is_ahead; then
-				local symbol=$FBP_GIT_DIRTY_AHEAD
+				local symbol=$SSP_GIT_DIRTY_AHEAD
 			elif $is_dirty && $is_behind; then
-				local symbol=$FBP_GIT_DIRTY_BEHIND
+				local symbol=$SSP_GIT_DIRTY_BEHIND
 			elif $is_dirty; then
-				local symbol=$FBP_GIT_DIRTY
+				local symbol=$SSP_GIT_DIRTY
 			elif $is_ahead && $is_behind; then
-				local symbol=$FBP_GIT_DIVERGED
+				local symbol=$SSP_GIT_DIVERGED
 			elif $is_ahead; then
-				local symbol=$FBP_GIT_AHEAD
+				local symbol=$SSP_GIT_AHEAD
 			elif $is_behind; then
-				local symbol=$FBP_GIT_BEHIND
+				local symbol=$SSP_GIT_BEHIND
 			else
-				local symbol=$FBP_GIT_SYNCED
+				local symbol=$SSP_GIT_SYNCED
 			fi
 
 
@@ -155,7 +155,7 @@ printSegment()
 	local next_background_color=$4
 	local font_effect=$5
 	if [ -z "$separator_char" ]; then local separator_char='\uE0B0'; fi
-
+	if [ -z "$segment_padding" ]; then local segment_padding=' '; fi
 
 
 	## COMPUTE COLOR FORMAT CODES
@@ -166,7 +166,7 @@ printSegment()
 
 
 	## GENERATE TEXT
-	printf "${text_format}${text}${separator_format}${separator_char}${no_color}"
+	printf "${text_format}${segment_padding}${text}${segment_padding}${separator_format}${separator_char}${no_color}"
 }
 
 
@@ -187,22 +187,22 @@ prompt_command_hook()
 
 
 	## UPDATE BASH PROMPT ELEMENTS
-	FBP_USER="$user"
-	FBP_HOST="$host"
-	FBP_PWD="$path"
+	SSP_USER="$user"
+	SSP_HOST="$host"
+	SSP_PWD="$path"
 	if [ -z "$git_branch" ]; then
-		FBP_GIT=""
+		SSP_GIT=""
 	else
-		FBP_GIT="$git_branch"
+		SSP_GIT="$git_branch"
 	fi
 
 
 
 	## CHOOSE PS1 FORMAT IF INSIDE GIT REPO
-	if [ ! -z "$(getGitBranch)" ] && $FBP_GIT_SHOW; then
-		PS1=$FBP_PS1_GIT
+	if [ ! -z "$(getGitBranch)" ] && $SSP_GIT_SHOW; then
+		PS1=$SSP_PS1_GIT
 	else
-		PS1=$FBP_PS1
+		PS1=$SSP_PS1
 	fi
 }
 
@@ -236,14 +236,14 @@ prompt_command_hook()
 	local ps1_input_format="\[$(getFormatCode       -c $font_color_input -b $background_input -e $texteffect_input)\]"
 	local ps1_input="${ps1_input_format} "
 
-	local ps1_user_git=$(printSegment " \${FBP_USER} " $font_color_user $background_user $background_host $texteffect_user)
-	local ps1_host_git=$(printSegment " \${FBP_HOST} " $font_color_host $background_host $background_pwd $texteffect_host)
-	local ps1_pwd_git=$(printSegment " \${FBP_PWD} " $font_color_pwd $background_pwd $background_git $texteffect_pwd)
-	local ps1_git_git=$(printSegment " \${FBP_GIT} " $font_color_git $background_git $background_input $texteffect_git)
+	local ps1_user_git=$(printSegment "\${SSP_USER}" $font_color_user $background_user $background_host $texteffect_user)
+	local ps1_host_git=$(printSegment "\${SSP_HOST}" $font_color_host $background_host $background_pwd $texteffect_host)
+	local ps1_pwd_git=$(printSegment "\${SSP_PWD}" $font_color_pwd $background_pwd $background_git $texteffect_pwd)
+	local ps1_git_git=$(printSegment "\${SSP_GIT}" $font_color_git $background_git $background_input $texteffect_git)
 
-	local ps1_user=$(printSegment " \${FBP_USER} " $font_color_user $background_user $background_host $texteffect_user)
-	local ps1_host=$(printSegment " \${FBP_HOST} " $font_color_host $background_host $background_pwd $texteffect_host)
-	local ps1_pwd=$(printSegment " \${FBP_PWD} " $font_color_pwd $background_pwd $background_input $texteffect_pwd)
+	local ps1_user=$(printSegment "\${SSP_USER}" $font_color_user $background_user $background_host $texteffect_user)
+	local ps1_host=$(printSegment "\${SSP_HOST}" $font_color_host $background_host $background_pwd $texteffect_host)
+	local ps1_pwd=$(printSegment "\${SSP_PWD}" $font_color_pwd $background_pwd $background_input $texteffect_pwd)
 	local ps1_git=""
 
 
@@ -252,15 +252,15 @@ prompt_command_hook()
 	## This is needed because each time the prompt updates,
 	## it must re-check the status of the current git repository,
 	## and to do so, it must remember the user's configuation
-	FBP_GIT_SHOW=$show_git
-	FBP_GIT_SYNCED=$git_symbol_synced
-	FBP_GIT_AHEAD=$git_symbol_unpushed
-	FBP_GIT_BEHIND=$git_symbol_unpulled
-	FBP_GIT_DIVERGED=$git_symbol_unpushedunpulled
-	FBP_GIT_DIRTY=$git_symbol_dirty
-	FBP_GIT_DIRTY_AHEAD=$git_symbol_dirty_unpushed
-	FBP_GIT_DIRTY_BEHIND=$git_symbol_dirty_unpulled
-	FBP_GIT_DIRTY_DIVERGED=$git_symbol_dirty_unpushedunpulled
+	SSP_GIT_SHOW=$show_git
+	SSP_GIT_SYNCED=$git_symbol_synced
+	SSP_GIT_AHEAD=$git_symbol_unpushed
+	SSP_GIT_BEHIND=$git_symbol_unpulled
+	SSP_GIT_DIVERGED=$git_symbol_unpushedunpulled
+	SSP_GIT_DIRTY=$git_symbol_dirty
+	SSP_GIT_DIRTY_AHEAD=$git_symbol_dirty_unpushed
+	SSP_GIT_DIRTY_BEHIND=$git_symbol_dirty_unpulled
+	SSP_GIT_DIRTY_DIVERGED=$git_symbol_dirty_unpushedunpulled
 
 
 
@@ -278,7 +278,7 @@ prompt_command_hook()
 	## Must be set in PS1
 	case $TERM in
 	xterm*|rxvt*)
-		local titlebar="\[\033]0;\${FBP_USER}@\${FBP_HOST}: \${FBP_PWD}\007\]"
+		local titlebar="\[\033]0;\${SSP_USER}@\${SSP_HOST}: \${SSP_PWD}\007\]"
 		;;
 	*)
 		local titlebar=""
@@ -288,8 +288,8 @@ prompt_command_hook()
 
 
 	## BASH PROMPT - Generate prompt and remove format from the rest
-	FBP_PS1="${titlebar}${vertical_padding}${ps1_user}${ps1_host}${ps1_pwd}${ps1_git}${ps1_input}"
-	FBP_PS1_GIT="${titlebar}${vertical_padding}${ps1_user_git}${ps1_host_git}${ps1_pwd_git}${ps1_git_git}${ps1_input}"
+	SSP_PS1="${titlebar}${vertical_padding}${ps1_user}${ps1_host}${ps1_pwd}${ps1_git}${ps1_input}"
+	SSP_PS1_GIT="${titlebar}${vertical_padding}${ps1_user_git}${ps1_host_git}${ps1_pwd_git}${ps1_git_git}${ps1_input}"
 
 
 
@@ -321,5 +321,5 @@ if [ "${BASH_SOURCE[0]}" == "${0}" ]; then
 elif [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
 	synth_shell_prompt
 	unset synth_shell_prompt
-
+	unset include
 fi
