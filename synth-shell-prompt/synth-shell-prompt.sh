b21e6d206500dc4c77c9c77bd4a2aee598739724
Andres Gongora <mail@andresgongora.com>
2021-02-04 12:35:31 +0100

    Fix include
    Signed-off-by: Andres Gongora <mail@andresgongora.com>

--- synth-shell-prompt/synth-shell-prompt.sh
+++ synth-shell-prompt/synth-shell-prompt.sh
@@ -41,7 +41,7 @@
 ##==============================================================================
 ##	EXTERNAL DEPENDENCIES
 ##==============================================================================
-[ "$(type -t include)" != 'function' ]&&{ include(){ { [ -z "$_IR" ]&&_IR="$PWD"&&cd $(dirname "${BASH_SOURCE[0]}")&&include "$1"&&cd "$_IR"&&unset _IR;}||{ local d=$PWD&&cd "$(dirname "$PWD/$1")"&&. "$(basename "$1")"&&cd "$d";}||{ echo "Include failed $PWD->$1"&&exit 1;};};}
+[ "$(type -t include)" != 'function' ]&&{ include(){ { [ -z "$_IR" ]&&_IR="$PWD"&&cd "$(dirname "${BASH_SOURCE[0]}")"&&include "$1"&&cd "$_IR"&&unset _IR;}||{ local d="$PWD"&&cd "$(dirname "$PWD/$1")"&&. "$(basename "$1")"&&cd "$d";}||{ echo "Include failed $PWD->$1"&&exit 1;};};}
 
 include '../bash-tools/bash-tools/color.sh'
 include '../bash-tools/bash-tools/shorten_path.sh'
